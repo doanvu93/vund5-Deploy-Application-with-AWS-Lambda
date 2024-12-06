29219c59b48af2dc306485e3e527485a083da8d8
Doan Vu <nguyendoanvu1993@gmail.com>
2024-12-06 16:30:20 +0700

    commit authen

--- starter/backend/src/lambda/auth/auth0Authorizer.mjs
+++ starter/backend/src/lambda/auth/auth0Authorizer.mjs
@@ -4,7 +4,7 @@ import { createLogger } from '../../utils/logger.mjs'
 
 const logger = createLogger('auth')
 
-const jwksUrl = 'https://test-endpoint.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-vund5-udacity.us.auth0.com/.well-known/jwks.json'
 
 export async function handler(event) {
   try {
@@ -43,10 +43,20 @@ export async function handler(event) {
 }
 
 async function verifyToken(authHeader) {
+  if (!authHeader) throw new Error('No authorization header');
+  if (!authHeader.toLowerCase().startsWith('bearer ')) throw new Error('Invalid authorization header');
   const token = getToken(authHeader)
   const jwt = jsonwebtoken.decode(token, { complete: true })
 
   // TODO: Implement token verification
+  try {
+    const res = await Axios.get(jwksUrl);
+    const keys = res.data.keys.find(key => key.kid === jwt.header.kid);
+    const certificate = certToPEM(keys.x5c[0]);
+    return jsonwebtoken.verify(token, certificate, {algorithms: ['RS256']});
+  } catch (error) {
+      logger.error('Token verification error', { error });
+  }
   return undefined;
 }
 
@@ -60,4 +70,10 @@ function getToken(authHeader) {
   const token = split[1]
 
   return token
+}
+
+// genarate certificate 
+function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  return `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
 }
